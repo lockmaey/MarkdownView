550df33366c4e12b3991a48d348d5fdb4a3000f9
lukman nulhakim abdul ghani <lockmaey@gmail.com>
2020-05-04 20:27:55 +0800

    update gradle version migrate to androidx update jquery plugins

--- library/src/androidTest/java/br/tiagohm/markdownviewx/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/br/tiagohm/markdownviewx/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package br.tiagohm.markdownview;
+package br.tiagohm.markdownviewx;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
---
+++ library/src/main/java/br/tiagohm/markdownviewx/js/JavaScript.java
@@ -0,0 +1,5 @@
+package br.tiagohm.markdownviewx.js;
+
+public interface JavaScript {
+    String toHTML();
+}
